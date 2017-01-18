6a0d6065e79c7f1e32208121787a560557f40398
Hugo Virgen <virgenherrera@gmail.com>
2017-01-18 12:07:51 -0600

    lv1 initial commit

--- calculator.js
+++ calculator.js
@@ -1,1 +1,15 @@
-//Your code HERE!
+/* ------- Tasks -------*/
+// 1 Update the page title with #content_title content
+//
+//
+// 	2.1 it's contructor will recives a string with operands and stores it in a private var named 'currentCalc'.
+// 	2.2 then must eval 'CurrentCalc'  and stores it in a private value named 'res'.
+// 	2.3 also the function must have a public getter named "OriginalCalc" wich exposes the private value stored in currentCalc
+// 	2.4 also it will have a public getter named "result" wich exposes the private value stored in res
+//
+//
+
+
+$(function(){
+	console.log('your code here!');
+});
